f378c2a9ffe12dc9f38ac347fadd7c39663c3c37
Nirokk <funaronicolas@gmail.com>
2025-11-06 21:05:50 -0300

    Enemys money drop and economy working

--- Assets/Scenes/Scripts/Enemies/Extras/EnemyData.cs
+++ Assets/Scenes/Scripts/Enemies/Extras/EnemyData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData", order = 1)]
 public class EnemyData : ScriptableObject
 {
+    public string type;
     public int speed;
     public int rotationSpeed;
     public int damage;
